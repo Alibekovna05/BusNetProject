fe4884c7facd369ba9195f8188f5b01232c5a738
aidanaMergem <aidanamergembaeva@gmail.com>
2024-04-25 23:34:59 +0600

    bus-flight-service

---
+++ bus-flight-service/src/main/java/kz/iitu/busnet/busflightservice/BusFlightServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.busnet.busflightservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusFlightServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusFlightServiceApplication.class, args);
+    }
+
+}
